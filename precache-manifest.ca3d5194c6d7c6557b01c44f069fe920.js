7e2ef972d5189319190a661301954fae1cb83e69
Son Le <sontdt69@gmail.com>
2020-02-18 10:24:25 +0700

    Updates

---
+++ precache-manifest.ca3d5194c6d7c6557b01c44f069fe920.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce5bb4986e4be0020d952b926e642e55",
+    "url": "/react-mini-minesweeper/index.html"
+  },
+  {
+    "revision": "01cb1bc676931661bcb9",
+    "url": "/react-mini-minesweeper/static/css/main.678f7658.chunk.css"
+  },
+  {
+    "revision": "22a49355c6cbea78dd66",
+    "url": "/react-mini-minesweeper/static/js/2.c0e1222b.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/react-mini-minesweeper/static/js/2.c0e1222b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01cb1bc676931661bcb9",
+    "url": "/react-mini-minesweeper/static/js/main.ff116ca5.chunk.js"
+  },
+  {
+    "revision": "43f37ddd9255361d758c",
+    "url": "/react-mini-minesweeper/static/js/runtime-main.05119645.js"
+  }
+]);
